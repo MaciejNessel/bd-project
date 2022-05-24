89814ae3cf459a511ca7c8a90e8cc5ce427d044b
MaciejNessel <maciej.nessel@gmail.com>
2022-05-25 01:47:57 +0200

    Fix filter bug

--- frontend/src/app/services/item-filter-pagination.service.ts
+++ frontend/src/app/services/item-filter-pagination.service.ts
@@ -11,33 +11,40 @@ export class ItemFilterPaginationService {
   items: Item[] = [];
   limit: Number = 3;
   currentPage = 0;
+  bodyFiltered = {};
+
   constructor(private server: ServerService, private router: Router) {
     this.loadMore();
   }
 
   async loadMore() {
+    const currBodyFiltered : any = this.bodyFiltered;
     const last = this.items;
     this.currentPage += 1;
-    const body = {
-      page: this.currentPage,
-      limit: this.limit
-    };
-    await this.server.fetchNextItems(body).subscribe((data: Item[]) => {
+
+    currBodyFiltered.page = this.currentPage;
+    currBodyFiltered.limit = this.limit;
+    console.log(currBodyFiltered);
+    await this.server.fetchNextItems(currBodyFiltered).subscribe((data: Item[]) => {
         // @ts-ignore
         this.items = this.items.concat(data.response)
       },
       (error => {
         console.log(error)
       }));
+
     return this.items == last;
   }
 
   newFilter(body: any) {
     this.currentPage = 1;
+    this.items = [];
     body.page = this.currentPage;
     body.limit = this.limit;
-    console.log(body);
+
     let bodyResult = Object.fromEntries(Object.entries(body).filter(([_, v]) => v != null && v != "Wybierz..."));
+    this.bodyFiltered = bodyResult;
+
     this.server.fetchNextItems(bodyResult).subscribe((data: Item[]) => {
         // @ts-ignore
         this.items = data.response},
